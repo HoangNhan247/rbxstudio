700338e58c83871840a4187e23bd62396eb0b613
h0void <69146295+HoangNhan247@users.noreply.github.com>
2025-08-25 17:24:00 +0700

    something lame

---
+++ Universal/womp.lua
@@ -0,0 +1,27 @@
+--[[
+
+Womp womp wompppp XD
+
+]]
+
+local p = game.Players.LocalPlayer
+local g = Instance.new('ScreenGui')
+local t = Instance.new('TextLabel')
+
+g.Parent = p.PlayerGui
+t.Parent = g
+t.Size = UDim2.new(.2,0,.1,0)
+t.Active = true
+t.Draggable = true
+t.TextScaled = true
+t.Text = 'Loading...'
+
+workspace.ChildAdded:Connect(function(c)
+    if game.Players:FindFirstChild(c.Name) then return end
+    pcall(function() 
+        local cT = c.Part.Info.AnimalOverhead:WaitForChild('DisplayName', 5)
+        if cT then 
+          t.Text = cT.ContextText 
+        end
+    end)
+end)
