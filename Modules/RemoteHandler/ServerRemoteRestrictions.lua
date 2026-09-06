eb85f16a00a7180a5e02fd7fe2e12b12636867a0
h0void <69146295+HoangNhan247@users.noreply.github.com>
2026-09-06 18:30:02 +0700

    Create ServerRemoteRestrictions.lua

---
+++ Modules/RemoteHandler/ServerRemoteRestrictions.lua
@@ -0,0 +1,89 @@
+--// Services
+local ServerScriptService = game:GetService("ServerScriptService")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local Staff = require(ServerScriptService.GameData.Staff)
+
+local module = {}
+
+--// Module functions
+
+function module.adminOnly()
+	return function (p: Player, data: {any})
+		return table.find(Staff.Admins, p.UserId) ~= nil
+	end
+end
+
+function module.teamOnly(t: {string})
+	return function (p: Player, data: {any})
+		for _, v in t do
+			if not Staff[v] then
+				warn('RR :: Unknown staff team: ', v)
+			end
+			
+			if table.find(Staff[v], p.UserId) ~= nil then
+				return true
+			end
+		end
+		
+		return false
+	end
+end
+
+-- Only allows players with the specified UserId
+function module.Whitelisted(id: number)
+	return function(p: Player, data: {any})
+		return p.UserId == id
+	end
+end
+
+-- Only allows data with correct types
+function module.typeChecking(t: {[string]: string})
+	return function (p: Player, data: {any})
+		for i, v in data do
+			if not t[i] then
+				warn('RR :: Unknown argument', i, t, data)
+			elseif typeof(v) ~= t[i] then
+				warn('RR :: Type checking failed', t, data)
+				return false
+			end
+		end
+		
+		return true
+	end
+end
+
+-- Only allows RIGHT AMOUNT of data with correct types
+function module.typeStrict(t: {[string]: string})
+	return function (p: Player, data: {any})
+		for i, v in data do
+			if ( not t[i] ) or ( typeof(v) ~= t[i] ) then
+				warn('RR :: Type stricted', t, data)
+				return false
+			end
+			
+			t[i] = nil
+		end
+		
+		if t ~= {} then
+			warn('RR :: Type missing', t, data)
+			return false
+		end
+		
+		return true
+	end
+end
+
+function module.forceData(t: {[string]: string})
+	return function (p: Player, data: {any})
+		for i, v in t do
+			if not data[i] or data[i] ~= t[i] then
+				return false
+			end
+		end
+		
+		return true
+	end
+end
+
+return module
